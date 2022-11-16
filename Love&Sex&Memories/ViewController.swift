603aecea560b1d9bde474ff3d4d62af1be420df7
Udenjordisk <udenjordisk@vk.com>
2022-11-16 23:09:07 +0600

    first commit

---
+++ Love&Sex&Memories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Love&Sex&Memories
+//
+//  Created by Кирилл on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
